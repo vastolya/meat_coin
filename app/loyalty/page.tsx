53ec1163b5218a4be80f9613fc0f679445b1237a
vastolya <paramee@bk.ru>
2026-05-22 19:03:41 +0700

    started loyalty page

--- app/loyalty/page.tsx
+++ app/loyalty/page.tsx
@@ -1,3 +1,20 @@
+import GridSection from '@/components/ui/GridSection'
+import H1Title from '@/components/ui/H1Title'
+import Image from 'next/image'
+
 export default function LoyaltyPage() {
-  return <main className="min-h-screen" />
+  return (
+    <>
+      <div className="bg-beige">
+        <GridSection className="pt-11 pb-30">
+          <H1Title className="text-accent col-span-8 col-start-5">
+            Программа лояльности meat_coin
+          </H1Title>
+          <div className="relative col-span-12 h-105 w-full rounded-lg">
+            <Image src={'/loyalty_01.webp'} alt="Программа лояльности" fill sizes="auto" />
+          </div>
+        </GridSection>
+      </div>
+    </>
+  )
 }
